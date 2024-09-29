70bdf3be9cd405c2924b76c1c7be6814b0384bf7
LTprograms <105554066+LTprograms@users.noreply.github.com>
2024-09-28 21:11:45 -0500

    fill speakers con json con pais

--- js/script.js
+++ js/script.js
@@ -8,7 +8,10 @@ function fillSpeakers(speakers) {
             <div class="col-sm-12 col-md-6 p-5">
                 <div class="bg-black p-4">
                     <img src="${speaker.foto}" alt="user" class="img-fluid rounded-circle d-block m-auto">
-                    <h5 class="my-4 text-white">${speaker.nombres} ${speaker.apellidos}</h5>
+                    <h5 class="my-4 text-white d-flex align-items-center gap-2">
+                        ${speaker.nombres} ${speaker.apellidos}
+                        <img width="30" height="30" src="https://img.icons8.com/emoji/48/${speaker.pais}-emoji.png" alt="${speaker.pais}-emoji"/>
+                    </h5>
                     <p class="text-gray">${speaker.perfil}</p>
                     <div class="d-flex justify-content-start align-items-center gap-2">
                         ${Object.keys(speaker.social_media).map(social => {
