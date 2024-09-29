4f1a3191eb0b2a7916ef1453a1e07bf0dff86fc1
LTprograms <105554066+LTprograms@users.noreply.github.com>
2024-09-28 20:59:09 -0500

    fill speakers con json

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const speakers = [
+    {
+        nombres: 'Alejandro',
+        apellidos: 'Saenz',
+        perfil: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        social_media: {
+            github: "#",
+            linkedin: "#",
+            twitter: "#",
+            // web: "#",
+        },
+        pais: "spain",
+        foto: "./img/user.png",
+    },
+    {
+        nombres: 'Fatz',
+        apellidos: 'Code',
+        perfil: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        social_media: {
+            github: "#",
+            linkedin: "#",
+            twitter: "#",
+            // web: "#",
+        },
+        pais: "peru",
+        foto: "./img/user.png",
+    },
+];
