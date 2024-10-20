947d0e90c4514f8efdc69775728a20e8fbbd584b
LTprograms <105554066+LTprograms@users.noreply.github.com>
2024-10-20 17:59:55 -0500

    home icons

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ const speakers = [
             youtube: "https://www.youtube.com/@flutterinastudio",
         },
         pais: "peru",
-        visible: false,
+        visible: true,
         talleres: [
         ],
         charlas: [
@@ -37,7 +37,7 @@ const speakers = [
             twitter: "https://twitter.com/javierblancoch",
         },
         pais: "peru",
-        visible: false,
+        visible: true,
         talleres: [
         ],
         charlas: [
@@ -63,7 +63,7 @@ const speakers = [
             youtube: "https://www.youtube.com/@maybemarhs",
         },
         pais: "peru",
-        visible: false,
+        visible: true,
         talleres: [
         ],
         charlas: [
@@ -117,7 +117,7 @@ const speakers = [
             youtube: "https://www.youtube.com/@yasminacodes",
         },
         pais: "spain",
-        visible: false,
+        visible: true,
         talleres: [
         ],
         charlas: [
